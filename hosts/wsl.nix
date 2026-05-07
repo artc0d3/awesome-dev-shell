28c2c41275ad065340a4791e3098a1a2b7edc83a
Artur Markiewicz <artur.m@posteo.de>
2026-05-07 12:30:57 +0000

    fix: X-specific libraries added to nix-ld. Allows Java to load system-dependent libs like lib/libawt_xawt.so

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -14,13 +14,20 @@ let
   # Nix-ld is required for tools that download and execute binaries, such as vfox.
   # NixOS doesn't provide a standard dynamic linker at /lib64/ld-linux-x86-64.so.2,
   # so externally downloaded ELF binaries can't run without it.
+  # The listed libraries are required for Node.js and JDK.
   nix-ld = {
     programs.nix-ld.enable = true;
     programs.nix-ld.libraries = with pkgs; [
+      alsa-lib
+      fontconfig
+      freetype
       stdenv.cc.cc.lib
+      xorg.libX11
+      xorg.libXext
+      xorg.libXi
+      xorg.libXrender
+      xorg.libXtst
       zlib
-      openssl
-      icu
     ];
   };
   podman = {
