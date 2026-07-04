a563f3b2eb078eaf61ee63ff306f9297c5a864c8
Artur Markiewicz <artur.m@posteo.de>
2026-07-04 18:34:09 +0200

    fix: Seeding .gitconfig

--- modules/dev-tools.nix
+++ modules/dev-tools.nix
@@ -20,6 +20,17 @@ in
       uv
     ];
 
+    # Seed an empty, user-editable ~/.gitconfig on install if one does not exist.
+    #
+    # Home Manager renders ~/.config/git/config as an immutable Nix-store symlink.
+    # If ~/.gitconfig doesn't exist, `git config` commands try to modify ~/.config/git/config which
+    # fails, because of immutability. Creating .gitconfig allows user to provide their local config.
+    home.activation.seedGitConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if [ ! -e "${config.home.homeDirectory}/.gitconfig" ]; then
+        run touch "${config.home.homeDirectory}/.gitconfig"
+      fi
+    '';
+
     programs.git = {
       enable = true;
       signing = {
