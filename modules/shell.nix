2816dba0082b4696340ddd5999526cc5e313a033
Artur Markiewicz <artur.m@posteo.de>
2026-07-05 15:22:45 +0200

    fix: ZSH syntax highlighting removed

--- modules/shell.nix
+++ modules/shell.nix
@@ -17,7 +17,6 @@ in
     programs.zsh = {
       enable = true;
       autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
       oh-my-zsh = {
         enable = true;
         plugins = [ "git" ];
